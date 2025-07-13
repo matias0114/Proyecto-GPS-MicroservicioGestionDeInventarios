46bb999fabb372475de4680772929415b8d7c9d7
matiasjaravega <mjarav@anticipa.cl>
2025-07-13 16:29:38 -0400

    Inicio Microservicio 4

--- Backend/src/main/java/com/ProyectoGPS/Backend/controller/InventarioController.java
+++ Backend/src/main/java/com/ProyectoGPS/Backend/controller/InventarioController.java
@@ -1,14 +1,12 @@
 package com.ProyectoGPS.Backend.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ProyectoGPS.Backend.dto.InventarioUploadRequest;
 import com.ProyectoGPS.Backend.service.InventarioService;
 
